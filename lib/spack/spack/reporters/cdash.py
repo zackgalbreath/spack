b18ad7aa450a025821e3dbcd0cb3edaac6d9d709
Zack Galbreath <zack.galbreath@kitware.com>
2019-11-18 15:00:25 -0500

    Avoid 'installed_from_binary_cache' KeyError in CDash reporter

--- lib/spack/spack/reporters/cdash.py
+++ lib/spack/spack/reporters/cdash.py
@@ -234,7 +234,8 @@ class CDash(Reporter):
             # from the binary cache.
             spec['packages'] = [
                 x for x in spec['packages']
-                if not x['installed_from_binary_cache']
+                if not 'installed_from_binary_cache' in x or
+                   not x['installed_from_binary_cache']
             ]
             for package in spec['packages']:
                 if 'stdout' in package:
